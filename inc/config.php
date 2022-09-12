02e7c1c443540e7fd23a9212ba2e166518b57166
studyguide96 <113317825+studyguide96@users.noreply.github.com>
2022-09-11 20:29:42 -0500

    Delete config.php

--- inc/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('DEBUG',true);
-define('DB_NAME', 'studyguide');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
-define('DB_HOST', '127.0.0.1:3306');
-
-define('SERVER','127.0.0.1:3306');
